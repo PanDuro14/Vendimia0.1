d2081bbc6e5f3d4eb1c70fcec9daceccc1d1c01f
Pan Duro <77323075+PanDuro14@users.noreply.github.com>
2025-07-29 19:13:11 -0600

    Initial commit

---
+++ app/src/main/java/com/example/vendimia01/ui/inventario/InventarioViewModel.java
@@ -0,0 +1,7 @@
+package com.example.vendimia01.ui.inventario;
+
+import androidx.lifecycle.ViewModel;
+
+public class InventarioViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
---
+++ app/src/main/java/com/example/vendimia01/ui/venta/VentasViewModel.java
@@ -0,0 +1,7 @@
+package com.example.vendimia01.ui.venta;
+
+import androidx.lifecycle.ViewModel;
+
+public class VentasViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
